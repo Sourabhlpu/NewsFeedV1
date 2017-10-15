f7573ec1070d0f16939f262dc002dba19b9a467e
Sourabh Pal <nick.tdr19@gmail.com>
2017-10-15 09:27:18 +0530

    Create a POJO class for bookmark, so that it can be easily sent to the realtime databaase.

---
+++ app/src/main/java/com/example/personal/newsfeeder/Bookmark.java
@@ -0,0 +1,25 @@
+package com.example.personal.newsfeeder;
+
+/**
+ * Created by personal on 10/15/2017.
+ */
+
+public class Bookmark {
+
+    private String mBookmarkUrl;
+
+    public Bookmark(String url)
+    {
+        mBookmarkUrl = url;
+    }
+
+    public String getmBookmarkUrl()
+    {
+        return mBookmarkUrl;
+    }
+
+    public void setmBookmarkUrl(String url)
+    {
+        mBookmarkUrl = url;
+    }
+}
